fd505838d3f6c1f119f975582a942232c18be182
Tarun Santhosh <tarun.santhosh@cavliwireless.com>
2023-02-06 11:08:10 +0530

    minor bug

--- utils/error.js
+++ utils/error.js
@@ -1,6 +1,6 @@
 const errorFunction = (error, msg, data) => {
     if (error) return { is_error: error, message: msg };
-    else return msg, data;
+    else return data;
 };
 
 module.exports = errorFunction;
